75f0d745c2877d168574edd4721c2d57c736c27a
Rachel McKinney <rachemck@yahoo.com>
2018-10-08 14:05:57 -0400

    First commit redo

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  console.log('test');
+  $('.single-item').slick({
+    dots: true,
+    //adaptiveHeight: true,
+
+  });
+
+  $("#recommend").flip();
+  $(".card").flip();
+});
