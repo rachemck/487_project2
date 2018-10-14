2476363ff05e5cd1fdb687a36b3b74a52a543786
Rachel McKinney <rachemck@yahoo.com>
2018-10-14 17:01:09 -0400

    adding last slide

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
   console.log('test');
 
-  $('.test').slick('slickGoTo', 2);
-
   $('.single-item').slick({
     dots: true,
     //adaptiveHeight: true,
@@ -12,3 +10,64 @@ $(document).ready(function(){
   $("#recommend").flip();
   $(".card").flip();
 });
+
+$('.ok-button').hide();
+
+        $('.edit-button,.ok-button').on('click',function(){
+
+        	$('.edit-button,.ok-button').toggle()
+        });
+
+        $('#quiz').quiz({
+          //resultsScreen: '#results-screen',
+          //counter: false,
+          //homeButton: '#custom-home',
+          counterFormat: 'Question %current of %total',
+          questions: [
+            {
+              'q': 'Is jQuery required for this plugin?',
+              'options': [
+                'Yes',
+                'No',
+                'Why'
+              ],
+              'correctIndex': 0,
+              'correctResponse': 'Good job, that was obvious.',
+              'incorrectResponse': 'Well, if you don\'t include it, your quiz won\'t work'
+            },
+            {
+              'q': 'How do you use it?',
+              'options': [
+                'Include jQuery, that\'s it!',
+                'Include jQuery and the plugin javascript.',
+                'Include jQuery, the plugin javascript, the optional plugin css, required markup, and the javascript configuration.'
+              ],
+              'correctIndex': 2,
+              'correctResponse': 'Correct! Sounds more complicated than it really is.',
+              'incorrectResponse': 'Come on, it\'s not that easy!'
+            },
+            {
+              'q': 'The plugin can be configured to require a perfect score.',
+              'options': [
+                'True',
+                'False'
+              ],
+              'correctIndex': 0,
+              'correctResponse': 'You\'re a genius! You just set allowIncorrect to true.',
+              'incorrectResponse': 'Why you have no faith!? Just set allowIncorrect to true.'
+            },
+            {
+              'q': 'How do you specify the questions and answers?',
+              'options': [
+                'MySQL database',
+                'In the HTML',
+                'In the javascript configuration'
+              ],
+              'correctIndex': 2,
+              'correctResponse': 'Correct! Refer to the documentation for the structure.',
+              'incorrectResponse': 'Wrong! Do it in the javascript configuration. You might need to read the documentation.'
+            }
+          ]
+        });
+
+        
